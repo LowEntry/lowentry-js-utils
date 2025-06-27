f2d011abf44149913c4c1c45e8a894ed01a826d6
Patrick Vane <patrick_vane@hotmail.com>
2025-06-27 21:32:47 +0200

    Improved the auto testing setup.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 export default {
-	globals:{
-		extensionsToTreatAsEsm:['.ts', '.js'],
-		'ts-jest':             {
-			useESM:true,
-		},
+	preset:              'ts-jest/presets/js-with-ts-esm',
+	globals:             {
+		extensionsToTreatAsEsm:['.ts', '.js', '.mjs', '.mjsx'],
 	},
-	preset: 'ts-jest/presets/js-with-ts-esm',
+	transform:           {
+		'^.+\\.[tj]sx?$|^.+\\.mjsx?$':['ts-jest', {useESM:true}],
+	},
+	moduleFileExtensions:['ts', 'tsx', 'js', 'jsx', 'mjs', 'mjsx', 'json', 'node'],
 };
